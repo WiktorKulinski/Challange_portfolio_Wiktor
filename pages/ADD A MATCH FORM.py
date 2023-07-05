d56348ad9ab8a4c1b1db30c23cf1becb9612fa13
WiktorKulinski <kulinski.wiktor@gmail.com>
2023-07-05 22:20:02 +0200

    selectors_tasks

---
+++ pages/ADD A MATCH FORM.py
@@ -0,0 +1,19 @@
+from pages.base_page import BasePage
+
+
+class MatchForm(BasePage):
+
+    //*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[1]/div/div/input
+    // *[ @ id = "__next"] / div[1] / main / div[2] / form / div[2] / div / div[5] / div / div / input
+    // *[ @ id = "__next"] / div[1] / main / div[2] / form / div[2] / div / div[6] / fieldset / div / label[2] / span[
+        1] / span[1] / input
+    // *[ @ id = "__next"] / div[1] / main / div[2] / form / div[3] / button[1]
+    // *[ @ id = "__next"] / div[1] / main / div[2] / form / div[3] / button[1]
+    // *[ @ id = "__next"] / div[1] / main / div[2] / form / div[2] / div / div[13] / div / div / input
+    // *[ @ id = "__next"] / div[1] / main / div[2] / form / div[2] / div / div[10] / div / div / input
+    // *[ @ id = "__next"] / div[1] / main / div[2] / form / div[2] / div / div[2] / div / div / input
+    // *[ @ id = "__next"] / div[1] / main / div[2] / form / div[2] / div / div[3] / div / div / input
+    // *[ @ id = "__next"] / div[1] / main / div[2] / form / div[2] / div / div[6] / fieldset / div / label[1] / span[
+        1] / span[1] / input
+
+    pass
---
+++ pages/dashboard.py
@@ -0,0 +1,16 @@
+from pages.base_page import BasePage
+
+
+class Dahsboaard(BasePage):
+    // span[text() = "Strona Główna"] - "Strona Główna" Button
+    // span[text() = "Gracze"]- "Gracze" Button
+    // span[text() = "English"]  - Change Language Button
+    // span[text() = "Sign out"]  - "Sign out" Button
+    // *[contains( @ title, "Logo Scouts Panel")] - Logo Graphic
+    //a[starts-with(@href, "/pl/players/add")] - hyper link to add player
+    //div[3]/div/div/a[1]/button/span[1]
+    //div[3]/div/div/a[2]/button/span[1]
+    //div[3]/div/div/a[3]/button/span[1]
+    //div[3]/div/div/a[4]/button/span[1]
+    //div[3]/div/div/a[5]/button/span[1]
+    pass
